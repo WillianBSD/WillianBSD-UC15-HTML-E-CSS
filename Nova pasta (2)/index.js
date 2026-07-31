1a0081598164129784c5d8ea916dc6aa03369e71
WillianBSD <spkfire@gmail.com>
2026-07-31 12:18:44 -0300

    Add files via upload

---
+++ Nova pasta (2)/index.js
@@ -0,0 +1,28 @@
+let nome
+let cargo
+let empresa
+let email
+let inicias
+
+let prompt=('inicias do seu nome?')
+console.log ('Bem vindo' + inicias)
+
+let produto
+let preco= 100
+let desconto = 20
+
+let = prompt (produto % desconto)
+let valorfinal =('Valor total=' + prompt)
+
+let senha = prompt ('Qual senha')
+let tamanho = senha.length
+
+if(tamanho >  10) {
+    alert("Senha forte")
+}
+if(tamanho > 8) {
+    alert('Senha média')
+}
+if(tamanho > 4) {
+    alert('Senha fraca')
+}
