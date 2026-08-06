5c6c2ed9363d27dd835f84c73e09d56c4a2abd59
WillianBSD <spkfire@gmail.com>
2026-08-06 10:30:31 -0300

    Add files via upload

---
+++ UC-15-JC-Calculadora/script.js
@@ -0,0 +1,21 @@
+let nome = prompt('Qual é o seu nome')
+let pesoStr = prompt(`Olá, $(nome)! Qual é o seu peso em kg?\n(use virgula ou ponto - ex: 75,5 ou 75.5)`;)
+let altStr = prompt('Qual é a sua altura em metros?\n(ex: 1,75 ou 1.75)')
+
+let peso = Number(pesoStr.replace(',', ','));
+let alt = Number (altStr.replace(',' , ','));
+
+console.log('Nome:', nome);
+console.log('Peso', peso, typeof peso);
+console.log('Altura' , alt, typeof alt);
+
+let imc = peso / (alt * alt);
+let imcFormatado = imc.toFixed('1');
+
+if (imc < 18.5) { console.log ('Abaixo do peso')}
+else if (imc < 25) {console.log ('Peso normal')}
+else if (imc < 30) {console.log ('Sobrepeso')}
+else if (imc < 35) {console.log('Obesiade grau 1')}
+else if (imc < 40) {console.log('Obesidade grau 2' )}
+else 
+   
