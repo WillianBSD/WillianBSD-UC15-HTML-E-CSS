eb00c171f02b7fd9cfddd719fdc78f0b0c49b9c5
WillianBSD <spkfire@gmail.com>
2026-08-20 10:33:02 -0300

    Add files via upload

---
+++ Atvidade/script.js
@@ -0,0 +1,38 @@
+//Exemplo
+let mensagem;
+if (nota >= 6) {
+    mensagem = 'Aprovado'
+} else {
+    mensagem = 'Reprovado'
+}
+
+const mensagem = nota > 6 ? 'Aprovado' : 'Reprovado';
+
+//Resposta
+
+let mensagem 
+if (temperatura >= 30) {
+mensagem = 'Quente'
+} else {
+    mensagem = 'Agradavel'
+}
+
+const mensagem  = temperatura >= 30 ? 'Quente' : 'Reprovado'
+
+let tipo;
+if (numero % 2=== 0) {
+    tipo= 'par'
+} else {
+    tipo= 'impar'
+}
+
+const mensagem = numero % 2 === 0? 'par' : 'Reprovado'
+
+let saudacao 
+if (hora < 12) {
+    saudacao ='Bom dia'
+} else {
+    saudacao ='Boa tarde/boa noite'
+}
+
+const mensagem = hora < 12 ? 'Bom dia' : 'Boa noite'
