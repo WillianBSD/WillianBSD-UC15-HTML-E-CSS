81f0de7ea6822cc847f9c36cfb1a3f575c25fba2
WillianBSD <spkfire@gmail.com>
2026-09-17 10:28:09 -0300

    Add files via upload

---
+++ DOM Uc 15/scrpit.js
@@ -0,0 +1,7 @@
+function mudarTexto () {
+    document.getElementById ('titulo').textContent = "Você clicou no botao!";
+}
+
+function mudarCor () {
+    document.getElementById ('Mensagem').style.color = "red";
+}
